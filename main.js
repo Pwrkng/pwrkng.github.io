751d8cd032996ac1992994dc89871fbba474521e
Pwrkng <augustdyrholm@gmail.com>
2021-08-29 18:08:41 +0200

    new cool colors

---
+++ main.js
@@ -0,0 +1,24 @@
+let load = () => {
+    for(let i=0;i<2500;i++){
+        let div = document.createElement("div");
+            div.id = 'div'+1;
+            div.classList.add('blockdiv');
+            div.classList.add('animate__animated');
+            div.classList.add('animate__backOutUp');
+            /*div.style.top = random(80)+10+'%';*/
+            div.style.top = '80%';
+            div.style.left = random(80)+10+'%';
+            div.style.animationDelay = '0.'+random(10)+'s';
+            div.style.animationDuration = random(10)+'s';
+            div.style.backgroundColor = '#'+randomColor();
+
+        document.body.appendChild(div);
+    
+    }
+}
+
+let random = (number) => {
+    return Math.floor(Math.random() * number);
+}
+
+let randomColor = () => Math.floor(Math.random()*16777215).toString(16);
